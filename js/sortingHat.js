625003d876078bd548ac585c757e0c42e9eaaa87
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-06-01 16:00:54 -0300

    sortingHatListo

--- js/sortingHat.js
+++ js/sortingHat.js
@@ -35,27 +35,7 @@ var hat = new Vue({
          }))
      },
 
-      //cargarVacunatorios() {
-        //axios.get("http://localhost:8080/grupo15-services/rest/puestovac/vac")
-          //.then((response => {
   
- //           console.log(`lista Vacunatorios: `, response.data)
-   //         this.listaVac = response.data
-  
-  
-         // }))
-       // },
-     // getData() {
-    
-    //    axios.get("http://localhost:8080/grupo15-services/rest/puestovac/asignado?vact="+this.idVac.toString()+"&date="+this.fecha.toString())
-  //        .then((response => {
-  
-         //   console.log(`VacPuesto: `, response.data)
-       //     this.vacPuesto = response.data
-  
-  
-     //     }))
-     
    
   
     }
